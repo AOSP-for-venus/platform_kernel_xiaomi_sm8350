878e5e7484af2c26deb09071c0ac2cb745aa0eaf
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
2024-01-29 12:39:28 +0800

    input: touchscreen: fts_spi: Implement single tap attribute
    - This is in accordance with our modernized ST2W implementation.
    - It sets single_tap_pressed to 1 if single click is detected.
    - Notify sysfs of the single_tap_pressed node change.
    
    [cyberknight777/ralph950412: Adapt to fts_spi driver]
    
    Change-Id: I149198f6ce8570c8649943201ac9e7630e55fd17
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -3618,6 +3618,15 @@ static ssize_t fts_secure_touch_show (struct device *dev, struct device_attribut
 	return scnprintf(buf, PAGE_SIZE, "%d", value);
 }
 #endif
+
+static inline ssize_t single_tap_pressed_get(struct device *dev,
+		               struct device_attribute *attribute,
+			       char *buffer)
+{
+       struct fts_ts_info *info = dev_get_drvdata(dev);
+       return scnprintf(buffer, PAGE_SIZE, "%i\n", info->single_tap_pressed);
+}
+
 static DEVICE_ATTR(fts_lockdown, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_lockdown_show, fts_lockdown_store);
 static DEVICE_ATTR(fwupdate, (S_IRUGO | S_IWUSR | S_IWGRP), fts_fwupdate_show,
@@ -3676,6 +3685,8 @@ static DEVICE_ATTR(gesture_mask, (S_IRUGO | S_IWUSR | S_IWGRP),
 static DEVICE_ATTR(gesture_coordinates, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_gesture_coordinates_show, NULL);
 #endif
+static DEVICE_ATTR(single_tap_pressed, S_IRUGO,
+		   single_tap_pressed_get, NULL);
 static DEVICE_ATTR(doze_time, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_doze_time_show, fts_doze_time_store);
 static DEVICE_ATTR(grip_enable, (S_IRUGO | S_IWUSR | S_IWGRP),
@@ -3728,6 +3739,7 @@ static struct attribute *fts_attr_group[] = {
 	&dev_attr_gesture_mask.attr,
 	&dev_attr_gesture_coordinates.attr,
 #endif
+	&dev_attr_single_tap_pressed.attr,
 	&dev_attr_selftest_info.attr,
 	&dev_attr_ms_raw.attr,
 	&dev_attr_ss_raw.attr,
@@ -4451,6 +4463,8 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 {
 	int value;
 	int needCoords = 0;
+	fts_info->single_tap_pressed = (event[2] == GEST_ID_SINGTAP) ? 1 : 0;
+	sysfs_notify(&fts_info->client->dev.kobj, NULL, "single_tap_pressed");
 #ifdef FTS_FOD_AREA_REPORT
 	int touch_area;
 	int fod_overlap;
