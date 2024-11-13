bfc4a7ef973fabaa1c077d0854e725ed2a370a09
Arian <arian.kulmer@web.de>
2024-11-13 14:35:50 +0800

    input: touchscreen: xiaomi: Notify sysfs when sensor enabled state changes
    Change-Id: I52e3294c27e71b48c45b6e72923df049bb91d42e

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -1020,6 +1020,12 @@ static ssize_t gesture_single_tap_enabled_store(struct device *dev,
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
 		return -EINVAL;
 
+	if (touch_data->getModeValue(Touch_Singletap_Gesture,
+				     GET_CUR_VALUE) != input) {
+		sysfs_notify(&xiaomi_touch_dev.dev->kobj, NULL,
+			     "gesture_single_tap_enabled");
+	}
+
 	touch_data->setModeValue(Touch_Singletap_Gesture, input);
 
 	return count;
@@ -1080,6 +1086,12 @@ static ssize_t gesture_double_tap_enabled_store(struct device *dev,
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
 		return -EINVAL;
 
+	if (touch_data->getModeValue(Touch_Doubletap_Mode,
+				     GET_CUR_VALUE) != input) {
+		sysfs_notify(&xiaomi_touch_dev.dev->kobj, NULL,
+			     "gesture_double_tap_enabled");
+	}
+
 	touch_data->setModeValue(Touch_Doubletap_Mode, input);
 
 	return count;
@@ -1131,6 +1143,12 @@ fod_longpress_gesture_enabled_store(struct device *dev,
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
 		return -EINVAL;
+
+	if (touch_data->getModeValue(Touch_Fod_Longpress_Gesture,
+				     GET_CUR_VALUE) != input) {
+		sysfs_notify(&xiaomi_touch_dev.dev->kobj, NULL,
+			     "fod_longpress_gesture_enabled");
+	}
 
 	touch_data->setModeValue(Touch_Fod_Longpress_Gesture, input);
 
